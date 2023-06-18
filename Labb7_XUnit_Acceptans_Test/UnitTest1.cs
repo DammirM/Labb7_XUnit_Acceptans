471b91315a873bd367bcef8f81225aadf1db1cd4
Damir Mehic <damir.mehic@edu.varberg.se>
2023-06-18 13:39:25 +0200

    Add project files.

---
+++ Labb7_XUnit_Acceptans_Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Labb7_XUnit_Acceptans_Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Labb7_XUnit_Acceptans
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
